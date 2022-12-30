158361e9082aace0d9badc0f440ec97bb931fdf5
Chahat Kalsi <chahat.ck88@gmail.com>
2022-12-30 22:27:50 +0530

    restcontroller and a simple endpoint added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController // DemoApplication can now serve endpoints
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping(value="/") // creating an endpoint
+	public String hello(){
+		return "Hello, World!";
 	}
 
 }
